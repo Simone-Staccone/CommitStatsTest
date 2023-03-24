2c8bb8f7b97a30afa2cd00cefc8a99c8ee4c1f7e
Simone Staccone <simone.staccone@virgilio.it>
2023-03-24 10:31:25 +0100

    Added security feature

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,10 +1,14 @@
 public class Printer {
     public static void main(String[] args){
         Printer.PrintNonSense();
-        System.out.println("Hello, this is a test project!");
+        Printer.PrintHello();
     }
 
     private static void PrintNonSense(){
         System.out.println("dfsggfusafiasds");
+    }
+
+    private static void PrintHello(){
+        System.out.println("Hello, this is a test project!");
     }
 }
