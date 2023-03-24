5695c82876fea6ed430c9837f47cb92bd75ae2e9
Simone Staccone <simone.staccone@virgilio.it>
2023-03-24 10:29:12 +0100

    Added nonsense print

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,5 +1,10 @@
 public class Printer {
     public static void main(String[] args){
+        Printer.PrintNonSense();
         System.out.println("Hello, this is a test project!");
+    }
+
+    private static void PrintNonSense(){
+        System.out.println("dfsggfusafiasds");
     }
 }
